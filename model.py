305592c90aa9c09e435e719afc7c1b91939c0320
B0bby31 <52623113+B0bby31@users.noreply.github.com>
2023-02-28 12:24:07 +0100

    Add simulation method

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ from collections import deque
 # indices: indices of removed nodes
 # notnat_ingoing: all the nodes that established a connection with a particular notnatted node
 # model_settings: the settings that can be used
-def one_step(G: nx.Graph, notnat: set, indices: deque, notnat_ingoing: dict, model_settings: ModelSettings, rand: Generator) -> nx.Graph:
+def one_step(G: nx.Graph, notnat: set, indices: deque, notnat_ingoing: dict, model_settings: ModelSettings, rand: Generator) -> (nx.Graph, set, deque, dict):
     if rand.random() < model_settings.q:  # Add node
         if len(indices) == 0: # index to use for next node
             nextnode = G.size()
@@ -30,7 +30,7 @@ def one_step(G: nx.Graph, notnat: set, indices: deque, notnat_ingoing: dict, mod
             notnat_ingoing[l].append(nextnode)
             G.add_edge(nextnode, l)
     else: # Exit node
-        selected_node = rand.integers(G.number_of_nodes())
+        selected_node = rand.choice(np.array(G.nodes))
         indices.append(selected_node)
         if selected_node in notnat: # rewire outgoing connections
             notnat.remove(selected_node)
@@ -41,17 +41,30 @@ def one_step(G: nx.Graph, notnat: set, indices: deque, notnat_ingoing: dict, mod
                     newcon = rand.choice(np.array(list(viable)))
                     G.add_edge(i,newcon)
         G.remove_node(selected_node) # remove node
-    return G
+    return G, notnat, indices, notnat_ingoing
 
 ### TODO: 
 # - ensure no indexing issues when sampling from a small notnat
 # - add proper handling of when there are not viable candidates to rewire too (in exit strategy)
 
-if __name__ == "__main__":
-    G = datasets.complete_graph(10)
-    modelsettings = ModelSettings(0.5,0.5,8,9)
-    rand = default_rng()
+# Simulate the evolution of the graph for {steps} runs
+# model_settings: specify parameters for simulation
+def simulate(model_settings: ModelSettings, steps: int, seed = None):
+    rand = default_rng(12345)
+    if seed is not None:
+        rand = default_rng(seed)
+    N = model_settings.outgoing + 1
+    G = datasets.complete_graph(N)
     notnat_ingoing = {}
-    for i in range(0,10):
-        notnat_ingoing[i] = [j for j in range(0,10) if j != i]
-    one_step(G, set(range(0,10)), deque(), notnat_ingoing ,modelsettings,rand)
+    for i in range(N):
+        notnat_ingoing[i] = [j for j in range(N) if j != i]
+    notnat = set(range(N))
+    indices = deque()
+    for i in range(steps):
+        G, notnat, indices, notnat_ingoing = one_step(G,notnat, indices, notnat_ingoing ,modelsettings, rand)
+    return G
+
+
+if __name__ == "__main__":
+    modelsettings = ModelSettings(0.9,0.5,8,9)
+    print(simulate(modelsettings, 10000))
